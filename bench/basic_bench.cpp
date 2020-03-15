c20ab8fc5b553e3e276ae600be8d8fba94995416
Luc <lruijsink@gmail.com>
2020-03-15 01:51:11 +0100

    More optimizations and benchmarked the same principle as filter but written explicitly

--- bench/basic_bench.cpp
+++ bench/basic_bench.cpp
@@ -6,15 +6,29 @@ auto get_values(int64_t n)
   auto res = std::vector<int>(n);
   for (int i = 0; i < n; i++)
     res[i] = i;
-  return std::move(res);
+  return res;
 }
 
 static void input_size_args(benchmark::internal::Benchmark* b) {
+  b->Arg(0);
   for (int i = 0; i <= 10; ++i)
-    b->Arg(1ll << (2 * i));
+    b->Arg(1ll << (1 * i));
 }
 
 auto is_odd = [] (auto x) { return x / 2 == 1; };
+
+static void bench_filter_view(benchmark::State& state)
+{
+  auto values = get_values(state.range(0));
+  for (auto _ : state)
+  {
+    for (auto v : values | lt3::range::filter(is_odd))
+    {
+      benchmark::ClobberMemory();
+    }
+  }
+}
+BENCHMARK(bench_filter_view)->Apply(input_size_args);
 
 static void bench_direct(benchmark::State& state)
 {
@@ -32,18 +46,25 @@ static void bench_direct(benchmark::State& state)
 }
 BENCHMARK(bench_direct)->Apply(input_size_args);
 
-static void bench_filter_view(benchmark::State& state)
+static void bench_explicit(benchmark::State& state)
 {
   auto values = get_values(state.range(0));
   for (auto _ : state)
   {
-    auto view = values | lt3::range::filter(is_odd);
-    for (auto v : view)
+    auto it = values.begin();
+    while (it != values.end() && !is_odd(*it))
+      it++;
+
+    while (it != values.end())
     {
+      it++;
+      while (it != values.end() && !is_odd(*it))
+        it++;
+
       benchmark::ClobberMemory();
     }
   }
 }
-BENCHMARK(bench_filter_view)->Apply(input_size_args);
+BENCHMARK(bench_explicit)->Apply(input_size_args);
 
 BENCHMARK_MAIN();
--- include/lt3/range/filter.h
+++ include/lt3/range/filter.h
@@ -10,8 +10,19 @@ template<class SourceRangeT, class PredicateT>
 struct filter_view
 {
   using source_range_type = SourceRangeT;
-  using source_iterator = typename source_range_type::iterator;
+  using source_iterator = decltype(std::declval<SourceRangeT>().begin());
+  using source_sentinel = decltype(std::declval<SourceRangeT>().end());
   using predicate_type = PredicateT;
+
+  struct sentinel
+  {
+    source_sentinel st;
+
+    bool operator!=(sentinel rh) const
+    {
+      return st != rh.st;
+    }
+  };
 
   struct iterator
   {
@@ -20,15 +31,10 @@ struct filter_view
 
     void operator++()
     {
-      if (it == view.source_end)
-        return;
-
       it++;
 
-      if (view.predicate(*it))
-        return;
-
-      operator++();
+      while (it != view.source_range.end() && !view.predicate(*it))
+        it++;
     }
 
     auto operator*() const
@@ -36,27 +42,27 @@ struct filter_view
       return *it;
     }
 
-    auto operator!=(iterator rh) const
+    auto operator!=(sentinel rh) const
     {
-      return it != rh.it;
+      return it != rh.st;
     }
   };
 
-  source_iterator source_begin;
-  source_iterator source_end;
-  predicate_type predicate;
+  source_range_type& source_range;
+  const predicate_type predicate;
 
-  auto begin()
+  auto begin() const
   {
-    auto it = source_begin;
-    while (it != source_end && !predicate(*it))
+    auto it = source_range.begin();
+    while (it != source_range.end() && !predicate(*it))
       it++;
+
     return iterator{ it, *this };
   }
 
-  auto end()
+  auto end() const
   {
-    return iterator{ source_end, *this };
+    return sentinel{ source_range.end() };
   }
 };
 
@@ -65,21 +71,20 @@ struct filter_piper
 {
   using predicate_type = PredicateT;
 
-  predicate_type predicate;
+  predicate_type const& predicate;
 
   template<class SourceRangeT>
-  friend auto operator|(SourceRangeT& source_range, filter_piper& piper)
+  friend auto operator|(SourceRangeT& source_range, filter_piper piper)
   {
     return filter_view<SourceRangeT, PredicateT>{
-      source_range.begin(),
-      source_range.end(),
+      source_range,
       piper.predicate
     };
   }
 };
 
 template<class PredicateT>
-auto filter(PredicateT predicate)
+constexpr auto filter(PredicateT const& predicate)
 {
   return filter_piper<PredicateT>{ predicate };
 }
